3ac1be03e8c19f8ce2bc12d86bb042864f10ef96
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-11 12:13:12 +0530

    terraform_vpc&docdb_rds_elastic_infra

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "aws_docdb_cluster" "docdb_cluster" {
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.docdb_sg.id]
-  storage_encrypted       = true
-  kms_key_id              = data.aws_kms_key.key.arn
+#  storage_encrypted       = true
+#  kms_key_id              = data.aws_kms_key.key.arn
 
   tags = merge(
     local.common_tags,
