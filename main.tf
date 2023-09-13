d72d0c5f73151753839f1f832a542535d5bbd655
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-13 14:25:51 +0530

    terrafrom apps and albs

--- main.tf
+++ main.tf
@@ -70,3 +70,21 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   )
 
 }
+
+resource "aws_ssm_parameter" "docdb_url_catalogue" {
+  name  = "${var.env}.catalogue.DOCDB_URL"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.docdb_user.value}:${data.aws_ssm_parameter.DB_ADMIN_PASS.value}@${aws_docdb_cluster.docdb.endpoint}:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url_user" {
+  name  = "${var.env}.user.DOCDB_URL"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.docdb_password.value}:${data.aws_ssm_parameter.DB_ADMIN_PASS.value}@${aws_docdb_cluster.docdb.endpoint}:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url" {
+  name  = "${var.env}.docdb.DOCDB_URL"
+  type  = "String"
+  value = aws_docdb_cluster.docdb_cluster.endpoint
+}
